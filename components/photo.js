3580b9d14096322fac079a9eb857ee5179c40e7b
rakesh <rakesh.tata@kp.org>
2020-06-08 22:03:10 -0700

    space format

--- components/photo.js
+++ components/photo.js
@@ -7,24 +7,23 @@ const Photo = ({ store,i }) => {
   const [, setTick] = useState(0);
     return (
       <View key={store[0]} style={{flexDirection:"row"}}>
-            <Lightbox underlayColor="white">
-                <Image
-                  source={{
-                    uri: JSON.parse(store[1]).uri,
-                  }}
-                  resizeMode="contain"
-                  style={{ width: 100, height: 100,justifyContent: 'flex-start' }}
-                />
-              </Lightbox>
-            <Text style={{ alignItems: 'center',justifyContent: 'flex-end'}}>
-            {`Step - ${i+1}`}
-            </Text>
-             <Button title="Delete" style={{justifyContent: 'flex-end'}} onPress={()=>{
-               AsyncStorage.removeItem(store[0],(err,result)=> {setTick(tick => tick + 1)})}}/>
-               <Button title="Upload" style={{justifyContent: 'flex-end'}} onPress={()=>PutImageToS3(store)}/>
-            </View>
-          )
-
+        <Lightbox underlayColor="white">
+            <Image
+              source={{
+                uri: JSON.parse(store[1]).uri,
+              }}
+              resizeMode="contain"
+              style={{ width: 100, height: 100,justifyContent: 'flex-start' }}
+            />
+          </Lightbox>
+        <Text style={{ alignItems: 'center',justifyContent: 'flex-end'}}>
+        {`Step - ${i+1}`}
+        </Text>
+         <Button title="Delete" style={{justifyContent: 'flex-end'}} onPress={()=>{
+           AsyncStorage.removeItem(store[0],(err,result)=> {setTick(tick => tick + 1)})}}/>
+        <Button title="Upload" style={{justifyContent: 'flex-end'}} onPress={()=>PutImageToS3(store)}/>
+      </View>
+    )
 }
 
 export default Photo;
